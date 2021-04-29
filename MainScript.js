8438cac068d4faf268d25d67e0e99f6b5336b3a0
Faridabuzayyad <farid.abuzayyad@gmail.com>
2021-04-29 23:04:45 +0300

    buttons functions created

--- MainScript.js
+++ MainScript.js
@@ -9,3 +9,25 @@ const aboutPage = () =>{
     $('#aboutContent').show();
     $("#about").disabled();
 };
+
+const ibrahimovice = $("#ibra");
+const neymar = $("#neymar");
+
+const arrayOfPlayers = [ibrahimovic , neymar];
+const mediaGalleryNext = () => {
+    for(i=0 ; i < arrayOfPlayers.length ; i++){
+        if(arrayofPlayers[i].fadeIn()){
+            arrayofPlayers[i].fadeOut()
+            arrayOfPlayers[i+1].fadeIn()
+        };
+    };
+};
+
+const mediaGalleryBack = () => {
+    for(i=0 ; i < arrayOfPlayers.length ; i--){
+        if(arrayofPlayers[i].fadeIn()){
+            arrayofPlayers[i].fadeOut()
+            arrayOfPlayers[i-1].fadeIn()
+        };
+    };
+};
