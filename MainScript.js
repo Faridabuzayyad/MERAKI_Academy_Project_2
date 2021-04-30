b8a308a9cd4062d6fc07967362684ab718018a31
Faridabuzayyad <farid.abuzayyad@gmail.com>
2021-04-30 16:44:36 +0300

    fixing the functionality of home page

--- MainScript.js
+++ MainScript.js
@@ -1,33 +1,53 @@
+// Hide other layers content , and disable Home page
 const goToHomePage =() =>{
     $('#aboutContent').hide();
+    $('#Home').removeAttr("href");
 }
 
-goToHomePage();
+//when the document is ready , invoke goToHomePage
+$(document).ready(goToHomePage)
 
-const aboutPage = () =>{
-    $('.bodyMain').hide();
+//go to about page + prevent default function ($(document).ready(goToHomePage)) from load again
+$("#about").click((e) =>{
+    e.preventDefault() 
+    $('.bodyMain1').hide();
+    $('.bodyMain2').hide();
     $('#aboutContent').show();
-    $("#about").disabled();
-};
+    $('#Home').attr("href", './LandingPage.html');
+});
 
-const ibrahimovice = $("#ibra");
+//selecting players (html elements)
+const ibrahimovic = $("#ibra");
 const neymar = $("#neymar");
+neymar.hide();
 
+//array of players in Media Gallery Page
 const arrayOfPlayers = [ibrahimovic , neymar];
+
+//next button functionality
 const mediaGalleryNext = () => {
     for(i=0 ; i < arrayOfPlayers.length ; i++){
-        if(arrayofPlayers[i].fadeIn()){
-            arrayofPlayers[i].fadeOut()
+        if(arrayOfPlayers[i].is(":visible") ){
+            arrayOfPlayers[i].fadeOut()
             arrayOfPlayers[i+1].fadeIn()
         };
     };
 };
 
+//back button functionality
 const mediaGalleryBack = () => {
     for(i=0 ; i < arrayOfPlayers.length ; i--){
-        if(arrayofPlayers[i].fadeIn()){
-            arrayofPlayers[i].fadeOut()
+        if(arrayOfPlayers[i].is(":visible")){
+            arrayOfPlayers[i].fadeOut()
             arrayOfPlayers[i-1].fadeIn()
         };
     };
 };
+
+//selecting buttons
+const nextButton = $("#next");
+const backButton = $("#back");
+
+//on click functionality
+nextButton.click(mediaGalleryNext);
+backButton.click(mediaGalleryBack);
